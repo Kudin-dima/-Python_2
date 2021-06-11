64815dc45bd4742e3d91c52430f9051ed65f84ee
Kudin-dima <79347418+Kudin-dima@users.noreply.github.com>
2021-06-11 06:25:51 +0300

    Update кодирование значений.py

--- кодирование значений.py
+++ кодирование значений.py
@@ -5,15 +5,12 @@ from sklearn import preprocessing
 input_labels = ['red','black','red','green','black','yellow','white']
 
 encoder = preprocessing.LabelEncoder() 
-# Этот кодировщик берёт столбец с категориальными данными, который был 
-# предварительно закодирован в признак, и создаёт для него несколько новых
-# столбцов. Числа заменяются на единицы и нули, в зависимости от того, 
-# какому столбцу какое значение присуще.
+
 print(encoder.fit(input_labels)) 
-# обучающей выборка через метод fit
+
 
 test_labels = ['green','red','black']
-encoded_values = encoder.transform(test_labels) # кодиование списка
+encoded_values = encoder.transform(test_labels) 
 print("\nLabels =", test_labels)
 
 print("Encoded values =", list(encoded_values))
@@ -21,8 +18,6 @@ print("Encoded values =", list(encoded_values))
 
 #декодирование случайного набора числел
 encoded_values = [3,0,4,1]
-decoded_list = encoder.inverse_transform(encoded_values) # обратное кодирование
+decoded_list = encoder.inverse_transform(encoded_values) 
 print("\nEncoded values =", encoded_values)
 
-print("Decoded labels =", list(decoded_list))
-# декодированные значения
